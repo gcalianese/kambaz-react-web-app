0948d5e9513c8bbf906f67c45c138b2a394d2aec
grace <calianese.g@northeastern.edu>
2025-01-20 00:23:19 -0500

    formatting

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -10,55 +10,91 @@ export default function AssignmentEditor() {
           The assignment is available online Submit a link to the landing page of
         </textarea>
         <p />
-        <table>
+        <table border="0">
           <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-points">Points</label>
+            <td align="right">
+              <label htmlFor="wd-points">Points </label>
             </td>
             <td>
-              <input id="wd-points" value={100} />
+                <input id="wd-points" value={100} />
             </td>
           </tr>
-        </table> <p/>
-        <label htmlFor="wd-group">Assignment Group </label>
-          <select id="wd-group">
-            <option value="ASSIGNMENTS">ASSIGNMENTS</option>
-          </select> <p/>
-        <label htmlFor="wd-display-grade-as">Display Grade As </label>
-        <select id="wd-display-grade-as">
-            <option value="Percentage">Percentage</option>
-        </select><p/>
-        <label htmlFor="wd-subsmission-type">Submission Type </label>
-        <select id="wd-submimssion-type">
-            <option value="Online">Online</option>
-        </select><p/>
-
-        <label htmlFor="wd-submission-type">Online Entry Options</label> <br/>
-        <input type="checkbox" name="text-entry" id="wd-submission-type"/>
-            <label htmlFor="submission-type">Text Entry</label> <br/>
-
-        <input type="checkbox" name="website-url" id="wd-submission-type"/>
-        <label htmlFor="submission-type">Website URL</label> <br/>
-
-        <input type="checkbox" name="media-recordings" id="wd-submission-type"/>
-        <label htmlFor="submission-type">Media Recordings</label> <br/>
-
-        <input type="checkbox" name="Student Annotation" id="wd-submission-type"/>
-        <label htmlFor="submission-type">Student Annotation</label> <br/>
-
-        <input type="checkbox" name="file-uploads" id="wd-submission-type"/>
-        <label htmlFor="submission-type">File Uploads</label> <br/> <br/>
-
-        <label htmlFor="wd-assign-to">Assign to </label> <br/>
-        <input id="wd-assign-to" value={"Everyone"} /> <p/>
-
-        <label htmlFor="wd-due-date">Due</label> <br/>
-        <input type="date" value="2024-05-13" id="wd-due-date"/><br/>
-
-        <p/>
-
-        <table>
           <tr>
+            <td align="right">
+                <label htmlFor="wd-group">Assignment Group </label>
+            </td>
+            <td>
+                <select id="wd-group">
+                    <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+                </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right">
+            <label htmlFor="wd-display-grade-as">Display Grade As </label>
+            </td>
+            <td>
+                <select id="wd-display-grade-as">
+                    <option value="Percentage">Percentage</option>
+                </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right">
+            <label htmlFor="wd-subsmission-type">Submission Type </label>
+            </td>
+            <td>
+                <select id="wd-submimssion-type">
+                    <option value="Online">Online</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <td>
+                    <label htmlFor="wd-submission-type">Online Entry Options</label> <br/>
+                    <input type="checkbox" name="text-entry" id="wd-submission-type"/>
+                    <label htmlFor="submission-type">Text Entry</label> <br/>
+    
+                    <input type="checkbox" name="website-url" id="wd-submission-type"/>
+                    <label htmlFor="submission-type">Website URL</label> <br/>
+
+                    <input type="checkbox" name="media-recordings" id="wd-submission-type"/>
+                    <label htmlFor="submission-type">Media Recordings</label> <br/>
+
+                    <input type="checkbox" name="Student Annotation" id="wd-submission-type"/>
+                    <label htmlFor="submission-type">Student Annotation</label> <br/>
+
+                   <input type="checkbox" name="file-uploads" id="wd-submission-type"/>
+                    <label htmlFor="submission-type">File Uploads</label> <br/> <br/>
+                </td>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+                <label htmlFor="wd-assign-to">Assign </label>
+            </td>
+            <td>
+                <label htmlFor="wd-assign-to">Assign to </label> <br/>
+                <input id="wd-assign-to" value={"Everyone"} />
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+                <label htmlFor="wd-due-date">Due</label> <br/>
+                <input type="date" value="2024-05-13" id="wd-due-date"/><br/>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+
+            </td>
+          </tr>
+          <tr>
+            <td></td>
             <td>
             <label htmlFor="wd-available-from">Available From </label>
             </td>
@@ -66,19 +102,26 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-available-until">Until</label>
             </td>
           </tr>
+          <td></td>
           <td>
             <input type="date" value="2024-05-06" id="wd-available-from"/>
           </td>
           <td>
             <input type="date" value="2024-05-20" id="wd-available-until"/><br/>
           </td>
-        </table> <br/> <br/>
-
-        <button type="button" id="wd-editor-cancel">Cancel</button>
-        <button type="button" id="wd-editor-save">Save</button>
+          <tr>
+            <td></td>
+            <td></td>
+            <td align="right">
+                <p/>
+                <button type="button" id="wd-editor-cancel">Cancel</button>
+                <button type="button" id="wd-editor-save">Save</button>
+            </td>
+          </tr>
+        </table>
+        
 
       </div>
 
       
   );}
-  
