bb8ec15e201e8bf207bdb66596a9547ef3e22650
grace <calianese.g@northeastern.edu>
2025-01-19 21:28:41 -0500

    assignments screen

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -266,12 +266,10 @@ src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap
 
   <h4>Anchor tag</h4>
     Please
-    <a href="https://www.lipsum.com" id="wd-lipsum"> click here </a>
-    to get dummy text<br/>
+    <a href="https://www.lipsum.com" id="wd-lipsum"> click here </a>to get dummy text<br/>
 
     Please
-    <a href="https://github.com/gcalianese/kambaz-react-web-app" id="wd-github"> click here </a>
-    to get to GitHub repo<br/>
+    <a href="https://github.com/gcalianese/kambaz-react-web-app" id="wd-github"> click here </a>to get to GitHub repo<br/>
 
       </div>
   );}
