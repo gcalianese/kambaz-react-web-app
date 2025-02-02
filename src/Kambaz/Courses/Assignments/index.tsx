1e1047af2b21e6bd5a0b3b38703c7bbc961aa014
grace <calianese.g@northeastern.edu>
2025-02-02 00:56:27 -0500

    format

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,40 +1,40 @@
 export default function Assignments() {
-    return (
-      <div id="wd-assignments">
-        <table>
-            <tr>
-                <td><input placeholder="Search for Assignments" id="wd-search-assignment" /></td>
-                <td><button id="wd-add-assignment-group">+ Group</button></td>
-                <td><button id="wd-add-assignment">+ Assignment</button></td>
-            </tr>
-        </table>
-        <h3 id="wd-assignments-title">
-          ASSIGNMENTS 40% of Total <button>+</button> </h3>
-        <ul id="wd-assignment-list">
-          <li className="wd-assignment-list-item">
-            <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A1 - ENV + HTML
-            </a> </li>
-            Multiple Modules | <b>Not available until</b> May 6 at 12:00am | <br/>
-            <b>Due</b> May 13 at 11:59pm | 100 pts
-          <li className="wd-assignment-list-item">
+  return (
+    <div id="wd-assignments">
+      <table>
+        <tr>
+          <td><input placeholder="Search for Assignments" id="wd-search-assignment" /></td>
+          <td><button id="wd-add-assignment-group">+ Group</button></td>
+          <td><button id="wd-add-assignment">+ Assignment</button></td>
+        </tr>
+      </table>
+      <h3 id="wd-assignments-title">
+        ASSIGNMENTS 40% of Total <button>+</button> </h3>
+      <ul id="wd-assignment-list">
+        <li className="wd-assignment-list-item">
           <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-                A2 - CSS + BOOTSTRAP
-                </a>
-          </li>
-          Multiple Modules | <b>Not available until</b> May 13 at 12:00am | <br/>
-          <b>Due</b> May 20 at 11:59pm | 100 pts
-          <li>
+            className="wd-assignment-link" >
+            A1 - ENV + HTML
+          </a> </li>
+        Multiple Modules | <b>Not available until</b> May 6 at 12:00am | <br />
+        <b>Due</b> May 13 at 11:59pm | 100 pts
+        <li className="wd-assignment-list-item">
           <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-                A3 - JAVASCRIPT + REACT
-                </a>
-          </li>
-          Multiple Modules | <b>Not available until</b> May 20 at 12:00am | <br/>
-          <b>Due</b> May 27 at 11:59pm | 100 pts
-        </ul>
-      </div>
-  );}
-  
+            className="wd-assignment-link" >
+            A2 - CSS + BOOTSTRAP
+          </a>
+        </li>
+        Multiple Modules | <b>Not available until</b> May 13 at 12:00am | <br />
+        <b>Due</b> May 20 at 11:59pm | 100 pts
+        <li>
+          <a href="#/Kambaz/Courses/1234/Assignments/123"
+            className="wd-assignment-link" >
+            A3 - JAVASCRIPT + REACT
+          </a>
+        </li>
+        Multiple Modules | <b>Not available until</b> May 20 at 12:00am | <br />
+        <b>Due</b> May 27 at 11:59pm | 100 pts
+      </ul>
+    </div>
+  );
+}
