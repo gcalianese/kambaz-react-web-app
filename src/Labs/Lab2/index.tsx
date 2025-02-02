d517e92b3a15af09cd22e12f2d3c5c50b10c5186
grace <calianese.g@northeastern.edu>
2025-02-02 15:13:08 -0500

    exercises

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,10 +11,17 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIcons from "./ReactIcons";
+import { Container } from "react-bootstrap";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
     return (
-        <div id="wd-lab2">
+        <Container>
             <h2>Lab 2 - Cascading Style Sheets</h2>
             <h3>Styling with the STYLE attribute</h3>
             <p>
@@ -81,5 +88,11 @@ export default function Lab2() {
             < GridLayout />
             < Flex />
             < ReactIcons />
-        </div>);
+            < BootstrapGrids />
+            < ScreenSizeLabel />
+            < BootstrapTables />
+            < BootstrapLists />
+            < BootstrapForms />
+            < BootstrapNavigation />
+        </Container>);
 }
