3801864346ac0e07b130d9ac76f7ac46db3ca364
grace <calianese.g@northeastern.edu>
2025-02-04 01:56:43 -0500

    style

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
     return (
         <div id="wd-account-navigation">
-            <Link to={'/Kambaz/Account/Signin'} > Signin </Link> <br />
-            <Link to={'/Kambaz/Account/Signup'} > Signup </Link> <br />
-            <Link to={'/Kambaz/Account/Profile'} > Profile </Link> <br />
+            <Link to={'/Kambaz/Account/Signin'} className="active border border-0 wd-black-text wd-account-nav-link d-block mb-2 me-3"> Signin </Link>
+            <Link to={'/Kambaz/Account/Signup'} className="border border-0 text-danger wd-account-nav-link d-block mb-2 me-3"> Signup </Link>
+            <Link to={'/Kambaz/Account/Profile'} className="border border-0 text-danger wd-account-nav-link d-block mb-2 me-3"> Profile </Link>
         </div>
     );
 }
