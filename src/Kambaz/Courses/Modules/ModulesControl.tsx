d67a652296158beadffc98548166f6de7ed58d7b
grace <calianese.g@northeastern.edu>
2025-02-04 17:15:45 -0500

    buttons

--- src/Kambaz/Courses/Modules/ModulesControl.tsx
+++ src/Kambaz/Courses/Modules/ModulesControl.tsx
@@ -29,11 +29,11 @@ export default function ModulesControls() {
                     </Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
-                Collapse All
-            </Button>
             <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-view-progress">
                 View Progress
+            </Button>
+            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
+                Collapse All
             </Button>
         </div>
     );
--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -22,31 +22,31 @@ export default function PeopleTable() {
                         <FaUserCircle className="me-2 fs-1 text-secondary" />
                         <span className="wd-first-name">Bruce</span>{" "}
                         <span className="wd-last-name">Wayne</span></td>
-                        <td className="wd-login-id">001234561S</td>
+                        <td className="wd-login-id">001234562S</td>
                         <td className="wd-section">S101</td>
                         <td className="wd-role">STUDENT</td>
-                        <td className="wd-last-activity">2020-10-01</td>
-                        <td className="wd-total-activity">10:21:32</td></tr>
+                        <td className="wd-last-activity">2020-11-02</td>
+                        <td className="wd-total-activity">15:32:43</td></tr>
 
                     <tr><td className="wd-full-name text-nowrap">
                         <FaUserCircle className="me-2 fs-1 text-secondary" />
                         <span className="wd-first-name">Steve</span>{" "}
                         <span className="wd-last-name">Rogers</span></td>
-                        <td className="wd-login-id">001234561S</td>
+                        <td className="wd-login-id">001234563S</td>
                         <td className="wd-section">S101</td>
                         <td className="wd-role">STUDENT</td>
-                        <td className="wd-last-activity">2020-10-01</td>
-                        <td className="wd-total-activity">10:21:32</td></tr>
+                        <td className="wd-last-activity">2020-10-02</td>
+                        <td className="wd-total-activity">22:32:43</td></tr>
 
                     <tr><td className="wd-full-name text-nowrap">
                         <FaUserCircle className="me-2 fs-1 text-secondary" />
                         <span className="wd-first-name">Natasha</span>{" "}
                         <span className="wd-last-name">Romanoff</span></td>
-                        <td className="wd-login-id">001234561S</td>
+                        <td className="wd-login-id">001234564S</td>
                         <td className="wd-section">S101</td>
-                        <td className="wd-role">STUDENT</td>
-                        <td className="wd-last-activity">2020-10-01</td>
-                        <td className="wd-total-activity">10:21:32</td></tr>
+                        <td className="wd-role">TA</td>
+                        <td className="wd-last-activity">2020-11-05</td>
+                        <td className="wd-total-activity">13:23:33</td></tr>
                 </tbody>
             </Table>
         </div>);
