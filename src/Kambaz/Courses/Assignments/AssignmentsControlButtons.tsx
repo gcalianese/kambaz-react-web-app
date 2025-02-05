f5f974169a2b2bbfeeab0a94567ddfeb2e463946
grace <calianese.g@northeastern.edu>
2025-02-05 13:32:40 -0500

    alignment

--- src/Kambaz/Courses/Assignments/AssignmentsControlButtons.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentsControlButtons.tsx
@@ -1,6 +1,6 @@
 import { BsPlus } from "react-icons/bs";
 import { IoEllipsisVertical } from "react-icons/io5";
-export default function ModuleControlButtons() {
+export default function AssignmentsControlButtons() {
     return (
         <div className="float-end">
             <label className="wd-assignment-percentage">40% of Total</label>
--- src/Kambaz/Courses/Modules/LessonControlButtons.tsx
+++ src/Kambaz/Courses/Modules/LessonControlButtons.tsx
@@ -3,8 +3,8 @@ import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 export default function LessonControlButtons() {
     return (
-        <div className="float-end">
-            <GreenCheckmark />
+        <div className="float-end wd-lesson-icon-container">
+            <span className="align-items-center"><GreenCheckmark /></span>
             <IoEllipsisVertical className="fs-4" />
         </div>);
 }
