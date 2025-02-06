16b0ca606d3d424e19b990409d7fdd6613b1e237
grace <calianese.g@northeastern.edu>
2025-02-05 20:12:02 -0500

    buttons

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
     return (
         <div id="wd-account-navigation">
-            <Link to={'/Kambaz/Account/Signin'} className="wd-black-text wd-account-nav-link active d-block mb-2 ps-1 me-3 "> Signin </Link>
-            <Link to={'/Kambaz/Account/Signup'} className="text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Signup </Link>
-            <Link to={'/Kambaz/Account/Profile'} className="text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Profile </Link>
+            <Link to={'/Kambaz/Account/Signin'} className="fs-5 wd-black-text wd-account-nav-link active d-block mb-2 ps-1 me-3 "> Signin </Link>
+            <Link to={'/Kambaz/Account/Signup'} className="fs-5  text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Signup </Link>
+            <Link to={'/Kambaz/Account/Profile'} className="fs-5  text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Profile </Link>
         </div>
     );
 }
--- src/Kambaz/Courses/Modules/ModulesControl.tsx
+++ src/Kambaz/Courses/Modules/ModulesControl.tsx
@@ -6,9 +6,11 @@ import { MdDoNotDisturbAlt } from "react-icons/md";
 export default function ModulesControls() {
     return (
         <div id="wd-modules-controls" className="text-nowrap d-flex flex-nowrap float-end">
-            <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Module
+            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
+                Collapse All
+            </Button>
+            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-view-progress">
+                View Progress
             </Button>
             <Dropdown className="float-end me-2">
                 <Dropdown.Toggle variant="secondary" size="lg" id="wd-publish-all-btn">
@@ -29,12 +31,11 @@ export default function ModulesControls() {
                     </Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-view-progress">
-                View Progress
+            <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn">
+                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                Module
             </Button>
-            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
-                Collapse All
-            </Button>
+
         </div>
     );
 }
