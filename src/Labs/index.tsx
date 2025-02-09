b772e71bce88053818ae614a003aee9bee7671f6
grace <calianese.g@northeastern.edu>
2025-02-09 13:43:48 -0500

    Lab 3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,9 +3,10 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+
 export default function Labs() {
   return (
-    <div>
+    <div className="p-3">
       <h1>Grace Calianese</h1>
       <h2>Web Dev CS 4550-02</h2>
       <h1>Labs</h1>
@@ -14,7 +15,7 @@ export default function Labs() {
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
     </div>
   );
