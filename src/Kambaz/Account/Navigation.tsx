38a276f1e17a0140fc946f3839117fab1bee3745
grace <calianese.g@northeastern.edu>
2025-02-14 17:43:03 -0500

    account nav

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
+    const links = [{ label: "Signin", path: "/Kambaz/Account/Signin" },
+    { label: "Signup", path: "/Kambaz/Account/Signup" },
+    { label: "Profile", path: "/Kambaz/Account/Profile" }];
+    const { pathname } = useLocation();
     return (
         <div id="wd-account-navigation">
-            <Link to={'/Kambaz/Account/Signin'} className="fs-5 wd-black-text wd-account-nav-link active d-block mb-2 ps-1 me-3 "> Signin </Link>
-            <Link to={'/Kambaz/Account/Signup'} className="fs-5  text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Signup </Link>
-            <Link to={'/Kambaz/Account/Profile'} className="fs-5  text-danger wd-account-nav-link d-block mb-2 ps-1 me-3"> Profile </Link>
+            {links.map((link) => (
+                <Link to={link.path}
+                id={`wd-account-${link.label}`}
+                className={`fs-5 wd-account-nav-link d-block mb-2 ps-1 me-3 ${pathname.includes(link.label) ? "active wd-black-text" : "text-danger"}`}> {link.label} </Link>
+            ))}
         </div>
     );
 }
--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import { useParams, useLocation } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
 import { courses } from "../Database";
 
 export default function CourseNavigation() {
