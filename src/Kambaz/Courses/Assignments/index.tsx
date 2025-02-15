ef82231b2d752fa2add735f74934223b6d17efcd
grace <calianese.g@northeastern.edu>
2025-02-15 13:36:31 -0500

    assignments

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -5,8 +5,14 @@ import { BsGripVertical } from "react-icons/bs";
 import AssignmentsControlButtons from "./AssignmentsControlButtons"
 import { HiMagnifyingGlass } from "react-icons/hi2";
 import { LuNotebookPen } from "react-icons/lu";
+import * as db from "../../Database";
+import { useParams } from "react-router";
+
 
 export default function Assignments() {
+  const assignments = db.assignments;
+  const { cid } = useParams();
+
   return (
     <div id="wd-assignments">
       <div className="wd-assignments-controls-container mb-5">
@@ -25,48 +31,25 @@ export default function Assignments() {
               <BsGripVertical className="me-2 fs-3 wd-assignment-drag" /> Assignments <AssignmentsControlButtons />
             </div>
             <ListGroup>
-              <ListGroup.Item className="wd-assignment p-3 ps-1 list-group-item rounded-0 d-flex align-items-center">
-                <span className="wd-assignment-icon-container">
-                  <BsGripVertical className="me-2 fs-3 wd-assignment-drag" />
-                  <LuNotebookPen className="wd-assignment-icon" />
-                </span>
-                <div className="wd-assignment-text flex-grow-1">
-                  <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-name">A1 - ENV + HTML </a>
-                  <label className="wd-assignment-description">
-                    <span className="text-danger"> Multiple Modules </span> | <span className="wd-bold">
-                      Not available until
-                    </span> May 6 at 12:00am | <span className="wd-bold"> Due </span> May 13 at 11:59pm | 100 pts</label>
-                </div>
-                <LessonControlButtons />
-              </ListGroup.Item>
-              <ListGroup.Item className="wd-assignment p-3 ps-1 list-group-item rounded-0 d-flex align-items-center">
-                <span className="wd-assignment-icon-container">
-                  <BsGripVertical className="me-2 fs-3 wd-assignment-drag" />
-                  <LuNotebookPen className="wd-assignment-icon" />
-                </span>
-                <div className="wd-assignment-text flex-grow-1">
-                  <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-name">A2 - CSS + BOOTSTRAP</a>
-                  <label className="wd-assignment-description">
-                    <span className="text-danger"> Multiple Modules </span> | <span className="wd-bold">
-                      Not available until
-                    </span> May 13 at 12:00am | <span className="wd-bold"> Due </span> May 20 at 11:59pm | 100 pts</label>
-                </div>
-                <LessonControlButtons />
-              </ListGroup.Item>
-              <ListGroup.Item className="wd-assignment p-3 ps-1 list-group-item rounded-0 d-flex align-items-center">
-                <span className="wd-assignment-icon-container">
-                  <BsGripVertical className="me-2 fs-3 wd-assignment-drag" />
-                  <LuNotebookPen className="wd-assignment-icon" />
-                </span>
-                <div className="wd-assignment-text flex-grow-1">
-                  <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-name">A3 - JAVASCRIPT + REACT</a>
-                  <label className="wd-assignment-description">
-                    <span className="text-danger"> Multiple Modules </span> | <span className="wd-bold">
-                      Not available until
-                    </span> May 20 at 12:00am | <span className="wd-bold"> Due </span> May 27 at 11:59pm | 100 pts</label>
-                </div>
-                <LessonControlButtons />
-              </ListGroup.Item>
+              {assignments
+                .filter((assignment: any) => assignment.course === cid)
+                .map((assignment: any) => (
+                  <ListGroup.Item className="wd-assignment p-3 ps-1 list-group-item rounded-0 d-flex align-items-center">
+                    <span className="wd-assignment-icon-container">
+                      <BsGripVertical className="me-2 fs-3 wd-assignment-drag" />
+                      <LuNotebookPen className="wd-assignment-icon" />
+                    </span>
+                    <div className="wd-assignment-text flex-grow-1">
+                      <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-name">{assignment.title}</a>
+                      <label className="wd-assignment-description">
+                        <span className="text-danger"> Multiple Modules </span> | <span className="wd-bold">
+                          Not available until
+                        </span> {assignment.available} | <span className="wd-bold"> Due </span> {assignment.due} | 100 pts</label>
+                    </div>
+                    <LessonControlButtons />
+                  </ListGroup.Item>
+                ))
+              }
             </ListGroup>
           </ListGroup.Item>
         </ListGroup>
