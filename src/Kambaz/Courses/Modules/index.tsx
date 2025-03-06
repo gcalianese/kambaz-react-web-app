44cb5c662ef9458767bbc39ca156e1978eab6bab
grace <calianese.g@northeastern.edu>
2025-03-05 21:34:41 -0500

    edit module

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router";
 import * as db from "../../Database";
 import ModulesControls from "./ModulesControl";
 import { v4 as uuidv4 } from "uuid";
+import { FormControl } from "react-bootstrap";
 
 
 export default function Modules() {
@@ -19,7 +20,12 @@ export default function Modules() {
   const deleteModule = (moduleId: string) => {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
-
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
 
   return (
     <div>
@@ -30,8 +36,19 @@ export default function Modules() {
           .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons moduleId={module._id}
-                  deleteModule={deleteModule} />
+                <BsGripVertical className="me-2 fs-3" />
+                {!module.editing && module.name}
+                {module.editing && (
+                  <FormControl className="w-50 d-inline-block"
+                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false });
+                      }
+                    }}
+                    defaultValue={module.name} />
+                )}
+                <ModuleControlButtons moduleId={module._id} deleteModule={deleteModule} editModule={editModule} />
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
