73eabff5c934404209ab4cba1bc81252c387ce2b
grace <calianese.g@northeastern.edu>
2025-03-06 17:33:16 -0500

    profile

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 export default function AccountNavigation() {
-    const links = [{ label: "Signin", path: "/Kambaz/Account/Signin" },
-    { label: "Signup", path: "/Kambaz/Account/Signup" },
-    { label: "Profile", path: "/Kambaz/Account/Profile" }];
     const { pathname } = useLocation();
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser
+        ? [{ label: "Profile", path: "/Kambaz/Account/Profile" }]
+        : [
+            { label: "Signin", path: "/Kambaz/Account/Signin" },
+            { label: "Signup", path: "/Kambaz/Account/Signup" },
+        ];
     return (
         <div id="wd-account-navigation">
             {links.map((link) => (
                 <Link to={link.path}
-                id={`wd-account-${link.label}`}
-                className={`fs-5 wd-account-nav-link d-block mb-2 ps-1 me-3 ${pathname.includes(link.label) ? "active wd-black-text" : "text-danger"}`}> {link.label} </Link>
+                    id={`wd-account-${link.label}`}
+                    className={`fs-5 wd-account-nav-link d-block mb-2 ps-1 me-3 ${pathname.includes(link.label) ? "active wd-black-text" : "text-danger"}`}> {link.label} </Link>
             ))}
         </div>
     );
