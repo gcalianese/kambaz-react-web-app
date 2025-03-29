b722baacddc69e73c0cbfc7660c301885bc90a9e
grace <calianese.g@northeastern.edu>
2025-03-29 16:21:16 -0400

    5.2.4

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -2,6 +2,7 @@ import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParamters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
@@ -19,6 +20,7 @@ export default function Lab5() {
             <PathParameters />
             <QueryParameters />
             <WorkingWithObjects />
+            <WorkingWithArrays />
  
         
         </div>
