368aae668251757efd68b6d3567d2a0b67b8fb99
grace <calianese.g@northeastern.edu>
2025-03-30 01:45:59 -0400

    5.3.2

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -3,16 +3,24 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 import { FormControl, Button } from "react-bootstrap";
+import * as client from "./client";
+
 export default function Profile() {
   const [profile, setProfile] = useState<any>({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const updateProfile = async () => {
+    const updatedProfile = await client.updateUser(profile);
+    dispatch(setCurrentUser(updatedProfile));
+  };
+
   const fetchProfile = () => {
     if (!currentUser) return navigate("/Kambaz/Account/Signin");
     setProfile(currentUser);
   };
-  const signout = () => {
+  const signout = async () => {
+    await client.signout();
     dispatch(setCurrentUser(null));
     navigate("/Kambaz/Account/Signin");
   };
@@ -26,19 +34,20 @@ export default function Profile() {
             onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
           <FormControl defaultValue={profile.password} id="wd-password" className="mb-2"
             onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
-          <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2"
+          <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2" placeholder="First Name"
             onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
-          <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2"
+          <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2" placeholder="Last Name"
             onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
           <FormControl defaultValue={profile.dob} id="wd-dob" className="mb-2"
             onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date" />
-          <FormControl defaultValue={profile.email} id="wd-email" className="mb-2"
+          <FormControl defaultValue={profile.email} id="wd-email" className="mb-2" placeholder="Email Address"
             onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
           <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
             className="form-control mb-2" id="wd-role">
             <option value="USER">User</option>            <option value="ADMIN">Admin</option>
             <option value="FACULTY">Faculty</option>      <option value="STUDENT">Student</option>
           </select>
+          <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
           <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
             Sign out
           </Button>
--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as client from "./client";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
 import { Form } from "react-bootstrap";
 export default function Signup() {
+  const [user, setUser] = useState<any>({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const signup = async () => {
+    const currentUser = await client.signup(user);
+    dispatch(setCurrentUser(currentUser));
+    navigate("/Kambaz/Account/Profile");
+  };
   return (
+
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <Form.Control placeholder="username" className="wd-username d-block mb-2" />
-      <Form.Control placeholder="password" type="password" className="wd-password mb-2 d-block" />
-      <Form.Control placeholder="verify password" type="password" className="wd-password-verify mb-2 d-block" />
-      <Link to="/Kambaz/Account/Profile" className="btn btn-primary w-100 mb-2"> Sign up </Link>
+      <Form.Control placeholder="username" className="wd-username d-block mb-2" onChange={(e) => setUser({ ...user, username: e.target.value })} />
+      <Form.Control placeholder="password" type="password" className="wd-password mb-2 d-block" onChange={(e) => setUser({ ...user, password: e.target.value })} />
+      <Link to="/Kambaz/Account/Profile" onClick={signup} className="btn btn-primary w-100 mb-2"> Sign up </Link>
       <Link to="/Kambaz/Account/Signin">Sign in</Link>
     </div>
+
   );
 }
