ac6a8a23db363131d42c483aa851cd78d2d6cba9
grace <calianese.g@northeastern.edu>
2025-03-29 23:58:16 -0400

    5.2.5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -4,6 +4,8 @@ import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
 import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
@@ -23,7 +25,8 @@ export default function Lab5() {
             <WorkingWithObjects />
             <WorkingWithArrays />
             <HttpClient />
- 
+            <WorkingWithObjectsAsynchronously />
+            <WorkingWithArraysAsynchronously />
         
         </div>
     );
