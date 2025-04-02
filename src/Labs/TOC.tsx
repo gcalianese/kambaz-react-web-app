e1dd6e6ca2ef7781c8d1cc52373f8395034e8d5c
grace <calianese.g@northeastern.edu>
2025-04-01 23:02:33 -0400

    cleanup

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -20,7 +20,7 @@ export default function TOC() {
       <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a5"
         active={pathname.includes("Lab5")}> Lab 5 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
-      <Nav.Item> <Nav.Link href="https://github.com/gcalianese/kambaz-react-web-app/tree/a4" target="_blank" id="wd-github" active={pathname.includes("github")}> My GitHub </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link href="https://github.com/gcalianese/kambaz-react-web-app/tree/a5" target="_blank" id="wd-github" active={pathname.includes("github")}> My GitHub </Nav.Link> </Nav.Item>
     </Nav>
   );
 }
