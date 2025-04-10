fa1db4c65f676416234a4711a705b667a00062e7
grace <calianese.g@northeastern.edu>
2025-04-09 21:36:38 -0400

    people popup

--- src/Kambaz/Courses/People/Details.tsx
+++ src/Kambaz/Courses/People/Details.tsx
@@ -15,9 +15,14 @@ export default function PeopleDetails() {
     useEffect(() => {
         if (uid) fetchUser();
     }, [uid]);
+
+    if (!uid) {
+        return null;
+    }
+
     return (
         <div className="wd-people-details position-fixed top-0 end-0 bottom-0 bg-white p-4 shadow w-25">
-            <button onClick={() => navigate(-1)} className="btn position-fixed end-0 top-0 wd-close-details">
+            <button onClick={() => navigate('/Kambaz/Account/Users')} className="btn position-fixed end-0 top-0 wd-close-details">
                 <IoCloseSharp className="fs-1" /> </button>
             <div className="text-center mt-2"> <FaUserCircle className="text-secondary me-2 fs-1" /> </div><hr />
             <div className="text-danger fs-4 wd-name"> {user.firstName} {user.lastName} </div>
--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -21,8 +21,8 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
                                 <td className="wd-full-name text-nowrap">
                                     {currentUser.role === "ADMIN" ? (<Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
                                         <FaUserCircle className="me-2 fs-1 text-secondary" />
-                                        <span className="wd-first-name">{user.firstName}{" "}</span>
-                                        <span className="wd-last-name">{user.lastName}</span>
+                                        <span className="wd-first-name text-danger">{user.firstName}{" "}</span>
+                                        <span className="wd-last-name text-danger">{user.lastName}</span>
                                     </Link>)
                                         : <label>  <FaUserCircle className="me-2 fs-1 text-secondary" />
                                             <span className="wd-first-name">{user.firstName}{" "}</span>
