f7380f446a3c1419cd63d0a2affdc75f004ee7bd
grace <calianese.g@northeastern.edu>
2025-04-09 23:38:55 -0400

    6.2.6.7

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import * as client from "./client";
 import { FormControl } from "react-bootstrap";
 import PeopleTable from "../Courses/People/Table";
+import { FaPlus } from "react-icons/fa";
 
 export default function Users() {
 
@@ -38,8 +39,26 @@ export default function Users() {
     useEffect(() => {
         fetchUsers();
     }, [uid]);
+
+    const createUser = async () => {
+        const user = await client.createUser({
+            firstName: "New",
+            lastName: `User${users.length + 1}`,
+            username: `newuser${Date.now()}`,
+            password: "password123",
+            email: `email${users.length + 1}@neu.edu`,
+            section: "S101",
+            role: "STUDENT",
+        });
+        setUsers([...users, user]);
+    };
+
     return (
         <div>
+            <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+                <FaPlus className="me-2" />
+                Users
+            </button>
             <h3>Users</h3>
             <FormControl onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="float-start w-25 me-2 wd-filter-by-name" />
