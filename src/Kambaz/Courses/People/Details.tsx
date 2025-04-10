7784255cbc5c6f0e52a8a97f651ccc5edf79b6be
grace <calianese.g@northeastern.edu>
2025-04-09 21:46:39 -0400

    6.2.6.5

--- src/Kambaz/Courses/People/Details.tsx
+++ src/Kambaz/Courses/People/Details.tsx
@@ -7,6 +7,12 @@ export default function PeopleDetails() {
     const { uid } = useParams();
     const [user, setUser] = useState<any>({});
     const navigate = useNavigate();
+    const deleteUser = async (uid: string) => {
+        await client.deleteUser(uid);
+        navigate('/Kambaz/Account/Users');
+    };
+
+
     const fetchUser = async () => {
         if (!uid) return;
         const user = await client.findUserById(uid);
@@ -29,5 +35,11 @@ export default function PeopleDetails() {
             <b>Roles:</b>           <span className="wd-roles">         {user.role}         </span> <br />
             <b>Login ID:</b>        <span className="wd-login-id">      {user.loginId}      </span> <br />
             <b>Section:</b>         <span className="wd-section">       {user.section}      </span> <br />
-            <b>Total Activity:</b>  <span className="wd-total-activity">{user.totalActivity}</span> </div>);
+            <b>Total Activity:</b>  <span className="wd-total-activity">{user.totalActivity}</span>
+
+            <hr />
+            <button onClick={() => deleteUser(uid)} className="btn btn-danger float-end wd-delete" > Delete </button>
+            <button onClick={() => navigate('/Kambaz/Account/Users')}
+                className="btn btn-secondary float-start float-end me-2 wd-cancel" > Cancel </button>
+        </div>);
 }
